772c5db53346cea03519b8bdc51806c64f04d1ac
Kai Ramuenke <kai.ramuenke@gmail.com>
2014-04-16 16:38:06 +1000

    using more readable matchers

--- test/rack/routes_test.rb
+++ test/rack/routes_test.rb
@@ -5,7 +5,7 @@ describe "routes" do
   describe "with no session" do
     it "should redirect all the things" do
       get '/foo'
-      last_response.status.must_equal 302
+      last_response.must_be :redirect?
     end
 
     it "should redirect to auth" do
@@ -15,17 +15,17 @@ describe "routes" do
 
     it "should not redirect auth routes" do
       get '/auth/'
-      last_response.status.must_equal 200
+      last_response.must_be :ok?
     end
 
     it "should not redirect js routes" do
       get '/js/jquery.min.js'
-      last_response.status.must_equal 200
+      last_response.must_be :ok?
     end
 
     it "should not redirect css routes" do
       get '/css/bootstrap.min.css'
-      last_response.status.must_equal 200
+      last_response.must_be :ok?
     end
   end
 end
